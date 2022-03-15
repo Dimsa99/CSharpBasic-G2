4b5439c9535ff509b0d96b311751a03fab079886
Dimitar.Josifovski <Dimitar.Josifovski@PALMYRA02>
2022-03-15 10:35:17 -0700

    Add project files.

---
+++ CSharpBasic-G2-Loops/CSharpBasic-G2-Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasic_G2_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpBasic-G2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpBasic_G2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Zdr grupa 2");
+
+            //Console.WriteLine(1);
+
+            //Console.WriteLine(1.11);
+            //Console.WriteLine(-1);
+            //Console.WriteLine("Zdravo grupa 2 \nova e primer vo nova linija");
+            // triangle
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("   * \n  * * \n  *** \n ******  \n********");
+
+
+
+
+            // stick man
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine(" *  *  *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine(" *     *");
+
+            Console.WriteLine("Please write some text");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
